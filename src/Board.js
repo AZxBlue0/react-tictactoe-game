70dbb09d523400b3cbf74592cb6d0bb45510354d
Blue Laptop <azxblue@o2.pl>
2023-02-07 15:00:29 +0100

    App setup

---
+++ src/Board.js
@@ -0,0 +1,22 @@
+
+export const Board = () => {
+    return (
+        <table>
+            <tr>
+                <td>X</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>O</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    )
+}
